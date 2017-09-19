ec1340c4fa1ca4dcdf306877e18c45b3bf2458cd
System <none>
2017-09-19 15:13:48 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.get("/unix-timestamp/:date", function(request, response){
   var url_body = url.parse(request.url);
   var date_args = [];
   date_args = request.params.date.split(' ');
-  response.json({"unix": request.para});
+  find_unix_time_from_given_date(date_args);
+  response.json({"unix": date_args});
 });
 
 app.get("/dreams", function (request, response) {
@@ -45,3 +46,40 @@ var dreams = [
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+function find_unix_time_from_given_date(date_args){
+  var month = 1;
+  var day = 1;
+  var year = 1970
+  switch(date_args[0].toLowerCase())
+  {
+    case "january" :  month = 1;
+                      break;
+    case "feburary" :  month = 2;
+                      break;
+    case "march" :  month = 3;
+                      break;
+    case "april" :  month = 4;
+                      break;
+    case "may" :  month = 5;
+                      break;
+    case "june" :  month = 6;
+                      break;
+    case "july" :  month = 7;
+                      break;
+    case "august" :  month = 8;
+                      break;
+    case "september" :  month = 9;
+                      break;
+    case "october" :  month = 10;
+                      break;
+    case "november" :  month = 11;
+                      break;
+    case "december" :  month = 12;
+                      break;
+      
+  }
+   day = parseInt(date_args[1]);
+  year = parseInt(date_args[2]);
+  
+}
