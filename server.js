5ee3223429f2c61ddb7672fdc3819a11e9673c69
System <none>
2017-09-19 15:24:48 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.get("/unix-timestamp/:date", function(request, response){
   var url_body = url.parse(request.url);
   var date_args = [];
   date_args = request.params.date.split(' ');
-  find_unix_time_from_given_date(date_args);
-  response.json({"unix": date_args});
+  
+  response.json({"unix": find_unix_time_from_given_date(date_args)});
 });
 
 app.get("/dreams", function (request, response) {
@@ -50,36 +50,14 @@ var listener = app.listen(process.env.PORT, function () {
 function find_unix_time_from_given_date(date_args){
   var month = 1;
   var day = 1;
-  var year = 1970
-  switch(date_args[0].toLowerCase())
-  {
-    case "january" :  month = 1;
-                      break;
-    case "feburary" :  month = 2;
-                      break;
-    case "march" :  month = 3;
-                      break;
-    case "april" :  month = 4;
-                      break;
-    case "may" :  month = 5;
-                      break;
-    case "june" :  month = 6;
-                      break;
-    case "july" :  month = 7;
-                      break;
-    case "august" :  month = 8;
-                      break;
-    case "september" :  month = 9;
-                      break;
-    case "october" :  month = 10;
-                      break;
-    case "november" :  month = 11;
-                      break;
-    case "december" :  month = 12;
-                      break;
-      
-  }
-   day = parseInt(date_args[1]);
+  var year = 1970;
+  month = date_args[0].toLowerCase();
+  day = parseInt(date_args[1]);
   year = parseInt(date_args[2]);
+  var parsedUnixTime = new Date(day + ' ' + month + ' ' +  year).getUnixTime();
+  // var parsedUnixTime = new Date('25 december 1995').getUnixTime();
+  return parsedUnixTime;
   
+
 }
+Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
